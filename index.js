a42be2eb83cf615c40b91162009b8ce9bd328ecb
Alex <alex.frene@gmail.com>
2016-10-09 18:16:32 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// Import third party libraries
+var https = require('https');
+var autobahn = require('autobahn');
+
+console.log("Starting Shield...");
+
+console.log("Shield started and ready to cast sensor data");
